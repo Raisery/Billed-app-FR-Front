e4e635ab1c1b7a34d3d6bd4bbcea416586c016f1
Lucas GERARD <lucas1.gerard@gmail.com>
2022-08-15 13:55:35 +0200

    final commit

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -30,30 +30,37 @@ export default class {
   getBills = () => {
     if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-          .map(doc => {
-            try {
-              return {
-                ...doc,
-                date: formatDate(doc.date),
-                status: formatStatus(doc.status)
+        .bills()
+        .list()
+        .then(snapshot => {
+          const bills = snapshot
+            .map(doc => {
+              try {
+                return {
+                  ...doc,
+                  date: formatDate(doc.date),
+                  status: formatStatus(doc.status)
+                }
+              } catch (e) {
+                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                // log the error and return unformatted date in that case
+                console.log(e, 'for', doc)
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status)
+                }
               }
-            } catch(e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
-              }
+            })
+          const res = []
+          for (let i in bills) {
+            if (bills[i].status) {
+              res.push(bills[i])
             }
-          })
-        return bills
-      })
+          
+        }
+          return res
+        })
     }
   }
 }
